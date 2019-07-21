18e99fd9fd55d4f3b9b1044cc55c9274b7b01888
blacky35 <tomomaki@lemon.plala.or.jp>
2019-07-21 09:38:43 +0900

    ver 0.1

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,17 @@
 /*
  *
  */
+$(ducument).on('pageinit', '#splash', function() {
+   $('#title0').text("bbb");
+    setTimeout(function() { $('#title11').addClass('animation'); }, 1000);
+    setTimeout(function() { $('#title12').addClass('animation'); }, 2000);
+    setTimeout(function() { $('#title13').addClass('animation'); }, 3000);
+    setTimeout(function() { $('#title1').addClass('animation'); }, 4000);
+    setTimeout(function() { $('#title21').addClass('animation'); }, 5000);
+    setTimeout(function() { $('.titleholder2').addClass('animation'); }, 6000);
+    setTimeout(function() { $('#title41').addClass('animation'); }, 7000);
+});
+
 var prevLatVal;
 var prevLngVal; 
 var dist;
