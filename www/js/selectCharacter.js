7e40248ce1b4b08d0d3eb318e312d45d91fd9d51
blacky35 <tomomaki@lemon.plala.or.jp>
2019-07-21 11:41:59 +0900

    ver 0.11

---
+++ www/js/selectCharacter.js
@@ -0,0 +1,23 @@
+// This is a JavaScript file
+
+$(document).on('pageinit', '#selectCharacter', function() {
+  console.log("selectCharacter");
+  $('.page__content')[0].style.fontSize = "30px";
+  $('.page__content')[0].style.color = "black";
+  $('.page__content')[0].style.textAlign = "center";
+/*
+  setTimeout(function() { $('#title11').addClass('animation'); }, 1000);
+  setTimeout(function() { $('#title12').addClass('animation'); }, 2000);
+  setTimeout(function() { $('#title13').addClass('animation'); }, 3000);
+  setTimeout(function() { $('#title1').addClass('animation'); }, 4000);
+  setTimeout(function() { $('#title21').addClass('animation'); }, 5000);
+  setTimeout(function() { $('.titleholder2').addClass('animation'); }, 6000);
+  setTimeout(function() { $('#title41').addClass('animation'); }, 7000);
+  setTimeout(function() {
+     $('#title51').addClass('animation');
+     $('#splash').on('click', function() {
+       appNavigator.resetToPage("selectCharacter.html", { animation: "slide" } );
+     })
+    }, 10000);
+*/
+});
---
+++ www/js/splash.js
@@ -0,0 +1,17 @@
+// This is a JavaScript file
+
+$(document).on('pageinit', '#splash', function() {
+  setTimeout(function() { $('#title11').addClass('animation'); }, 1000);
+  setTimeout(function() { $('#title12').addClass('animation'); }, 2000);
+  setTimeout(function() { $('#title13').addClass('animation'); }, 3000);
+  setTimeout(function() { $('#title1').addClass('animation'); }, 4000);
+  setTimeout(function() { $('#title21').addClass('animation'); }, 5000);
+  setTimeout(function() { $('.titleholder2').addClass('animation'); }, 6000);
+  setTimeout(function() { $('#title41').addClass('animation'); }, 7000);
+  setTimeout(function() {
+     $('#title51').addClass('animation');
+     $('#splash').on('click', function() {
+       appNavigator.resetToPage("selectCharacter.html", { animation: "slide" } );
+     })
+    }, 10000);
+});
